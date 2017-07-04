38d23f6fad0ba6e52e3a73c3340c2c970174712b
Stephen Loquet <sloquet@gmail.com>
2017-07-03 17:55:15 -0700

    complete days projects

---
+++ w5d3/first_js.js
@@ -0,0 +1,20 @@
+
+const madLib = (verb, adj, noun) => {
+  console.log("We shall " + verb + " the " + adj + " " + noun + ".");
+}
+
+madLib("run", "sparkling", "jewels");
+
+// const isSubstring = (searchString, subString) => {
+//   console.log(searchString.includes(subString));
+// }
+
+const isSubstring = (searchString, subString) => {
+  console.log(searchString.indexOf(subString) !== -1)
+}
+
+isSubstring("time to program", "time")
+
+const fizzBuzz = (array) => {
+  
+}
